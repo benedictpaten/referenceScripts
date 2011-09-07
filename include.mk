44af53d92394d9cba19b0e1a8256876b118987f0
Benedict <benedictpaten@gmail.com>
2011-09-07 12:16:13 -0700

    Fix

--- include.mk
+++ include.mk
@@ -2,7 +2,6 @@ jobTreeFlags = --batchSystem parasol --logDebug --retryCount 0 --maxThreads 4 --
 #jobTreeFlags = --batchSystem singleMachine --maxThreads 2 --logDebug --retryCount 0
 configFile=${libPath}/config_fast.xml
 minimumNsForScaffoldGap=15
-minimumLengthOfFragment=100
 sampleNumber=1000000
 
 #########
