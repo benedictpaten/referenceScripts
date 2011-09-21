327f670c1e991e584f9712d8cdbb2e3c6c5a217f
Benedict <benedictpaten@gmail.com>
2011-09-21 11:01:57 -0700

    Fixing stuff

--- src/scripts/filterNonComponentSequences.py
+++ src/scripts/filterNonComponentSequences.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as ET
 from sonLib.bioio import fastaRead, fastaWrite
 i = set([ i for i in ET.parse(sys.argv[1]).getroot().text.split() ])
 fH = open(sys.argv[3], 'w')
-for name, sequence in fastaRead(open(sys.arv[2], 'r')):
+for name, sequence in fastaRead(open(sys.argv[2], 'r')):
         if name not in i:
             fastaWrite(fH, name, sequence)
 fH.close()
