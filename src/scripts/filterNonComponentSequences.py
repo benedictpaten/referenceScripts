e023ce5c242651eea854f2f83d0093cd1ea28660
Benedict <benedictpaten@gmail.com>
2011-09-21 10:42:02 -0700

    Adding scripts to do alignments filtered for sequences that don't fit

---
+++ src/scripts/filterNonComponentSequences.py
@@ -0,0 +1,9 @@
+import sys
+import xml.etree.ElementTree as ET
+from sonLib.bioio import fastaRead, fastaWrite
+i = set([ i for i in ET.parse(sys.argv[1]).getroot().text.split() ])
+fH = open(sys.argv[3], 'w')
+for name, sequence in fastaRead(open(sys.arv[2], 'r')):
+        if name not in i:
+            fastaWrite(fH, name, sequence)
+fH.close()
