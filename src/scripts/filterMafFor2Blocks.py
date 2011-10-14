30ba093026760cde3ec07d6fcbaeb15cdc0e03b6
Benedict <benedictpaten@gmail.com>
2011-10-14 11:56:06 -0700

    Fixing script

--- src/scripts/filterMafFor2Blocks.py
+++ src/scripts/filterMafFor2Blocks.py
@@ -2,12 +2,15 @@ import sys
 l = []
 fH = open(sys.argv[2], 'w')
 for line in open(sys.argv[1], 'r').readlines():
-    if line[:2] == 'a ':
-        if len(l) == 4:
-            for line in l:
-                fH.write(l)
+    if line[:1] == 'a':
+        if len(l) == 4 and "hg19" in " ".join(l):
+            print "making block"
+            for i in l:
+                fH.write(i)
             fH.write("\n")
+        else:
+            print "invalid block", len(l)
         l = [ line ]
-    elif line[:2] == 's ':
-        l.append(l)
+    elif line[:1] == 's':
+        l.append(line)
 fH.close()
