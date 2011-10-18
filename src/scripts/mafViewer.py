6c819bf196647734758344fc2e3a15bce2080775
benedictpaten <benedictpaten@gmail.com>
2011-10-17 19:56:20 -0700

    Adding selection to maf viewer

--- src/scripts/mafViewer.py
+++ src/scripts/mafViewer.py
@@ -1,6 +1,7 @@
 import sys
 
 fH = open(sys.argv[2], 'w')
+seqs = sys.argv[3:]
 pL = {}
 l = []
 for line in open(sys.argv[1], 'r'):
@@ -8,6 +9,12 @@ for line in open(sys.argv[1], 'r'):
         if l != []:
             pL2 = {}
             l.sort()
+            def fn(i):
+                for name in seqs:
+                    if name in i:
+                        return True
+                return False
+            l = [ i for i in l if len(seqs) == 0 or fn(i) ]
             fH.write("%i\n" % len(l))
             for i in l:
                 i = i.split()
@@ -21,7 +28,8 @@ for line in open(sys.argv[1], 'r'):
                         b2 = 1
                         if int(i[2]) == int(pL[i[1]][2]) + int(pL[i[1]][3]):
                             b3 = 1
-                fH.write("%s\t%s\t%i\t%i\t%i\t%s\n" % (i[1][:10] + " " * (10 - len(i[1][:10])), i[2], b, b2, b3, i[-1][:30]))
+                fH.write("%s\t%s\t%i\t%i\t%i\t%s\n" % \
+                         (i[1][:10] + " " * (10 - len(i[1][:10])), i[2], b, b2, b3, i[-1][:30]))
             fH.write("\n")
             l = []
             pL = pL2
