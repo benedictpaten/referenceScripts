71093f88d6e6bf8fcb1da0cedeb517d5cab6abdd
Benedict <benedictpaten@gmail.com>
2012-04-10 09:23:36 -0700

    Slimmed down number of different configurations that get run for MHC tests

--- main/include.mk
+++ main/include.mk
@@ -12,18 +12,21 @@ dataDir=${dataPath}/${experimentName}
 sequences=${dataDir}/panTro3 ${dataDir}/NA12878 ${dataDir}/NA12892 ${dataDir}/NA19239 ${dataDir}/apd ${dataDir}/dbb ${dataDir}/mann ${dataDir}/nigerian ${dataDir}/qbl ${dataDir}/venter ${dataDir}/yanhuang ${dataDir}/NA19238 ${dataDir}/NA19240 ${dataDir}/cox ${dataDir}/hg19 ${dataDir}/mcf ${dataDir}/ssto ${dataDir}/cgIndelContigs.fa ${dataDir}/1kIndelContigs.fa ${dataDir}/snp135IndelContigs.fa
 outputDir=${outputPath}/main/${experimentName}
 referenceSpecies=reference hg19
-minimumBlockDegreeRange=1 2 3 4 5 6 7 8
+minimumBlockDegreeRange=1 2 
+#3 4 5 6 7 8
 #algorithms: greedy maxCardinality maxWeight blossom5
 referenceAlgorithms = maxWeight
 baseLevel = 1
 maxNumberOfChains = 10000
 blastAlignmentStrings = 
 #--nogapped %  % --step=10 --seed=match12 --notransition --mismatch=2,100 --match=1,5 --nogapped
-theta = 0.0 0.0000001 0.000001 0.00001 0.01
+theta = 0.00001
+#theta = 0.0 0.0000001 0.000001 0.00001 0.01
 useSimulatedAnnealing = 0
 permutations = 100
 outgroupEvent=panTro3
-gapGamma = 0.2
+gapGamma = 0.0
+#gapGamma = 0.2
 constraints=${dataDir}/constraints.cig
 
 include ${rootPath}/include.mk
