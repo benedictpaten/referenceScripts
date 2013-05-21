ab28c752be419a1e0a0fc65e8c3f24c8fea2c651
benedictpaten <benedictpaten@gmail.com>
2013-05-21 14:12:39 -0700

    -

--- main/include.mk
+++ main/include.mk
@@ -7,7 +7,7 @@ outputPath = ${rootPath}output
 #(panTro2:0.007060,(cox:0.000890,((qbl:0.000680,mann:0.000520):0.000270,(((mcf:0.001010,dbb:0.000790):0.000210,apd:0.000740):0.000040,(ssto:0.001000,hg19:0.000800):0.000260):0.000040):0.000100):0.005850);
 #Base tree (panTro2:0.007060,x:0.005850);
 
-newickTree=(panTro3:0.01291,NA12878:0.002000,NA12892:0.002000,NA19239:0.002000,apd:0.002000,dbb:0.002000,mann:0.002000,nigerian:0.002000,qbl:0.002000,venter:0.002000,yanhuang:0.002000,NA19238:0.002000,NA19240:0.002000,cox:0.002000,hg19:0.002000,mcf:0.002000,ssto:0.002000,cgIndelContigs.fa:0.002,1kIndelContigs.fa:0.002,snp135IndelContigs.fa:0.002):reference;
+newickTree=(panTro3:0.01291,NA12878:0.002000,NA12892:0.002000,NA19239:0.002000,apd:0.002000,dbb:0.002000,mann:0.002000,nigerian:0.002000,qbl:0.002000,venter:0.002000,yanhuang:0.002000,NA19238:0.002000,NA19240:0.002000,cox:0.002000,hg19:0.002000,mcf:0.002000,ssto:0.002000,cgIndelContigs.fa:0.002,1kIndelContigs.fa:0.002,snp135IndelContigs.fa:0.002)reference;
 dataDir=${dataPath}/${experimentName}
 sequences=${dataDir}/panTro3 ${dataDir}/NA12878 ${dataDir}/NA12892 ${dataDir}/NA19239 ${dataDir}/apd ${dataDir}/dbb ${dataDir}/mann ${dataDir}/nigerian ${dataDir}/qbl ${dataDir}/venter ${dataDir}/yanhuang ${dataDir}/NA19238 ${dataDir}/NA19240 ${dataDir}/cox ${dataDir}/hg19 ${dataDir}/mcf ${dataDir}/ssto ${dataDir}/cgIndelContigs.fa ${dataDir}/1kIndelContigs.fa ${dataDir}/snp135IndelContigs.fa
 outputDir=${outputPath}/main/${experimentName}
