f3e2a9d7de5b0bdd36028f2a5638b4a6f216359d
Benedict <benedictpaten@gmail.com>
2013-06-20 16:07:45 -0700

    -

--- include.mk
+++ include.mk
@@ -1,5 +1,5 @@
 #jobTreeFlags = --batchSystem parasol --logDebug --retryCount 0 --maxThreads 4 --defaultMemory 4294967296
-jobTreeFlags = --batchSystem singleMachine --maxThreads 20 --logDebug --retryCount 0 --maxJobs 20
+jobTreeFlags = --batchSystem singleMachine --maxThreads 20 --logDebug --retryCount 0 --maxCpus 20
 configFile=${libPath}/config_fast.xml
 minimumNsForScaffoldGap=15
 sampleNumber=1000000
@@ -7,10 +7,13 @@ sampleNumber=1000000
 batchSystemForAlignments = singleMachine
 databaseHost = localhost
 parasolCommandForAlignment = parasol
+extraJobTreeArgs = 
 
 #batchSystemForAlignments = parasol singleMachine 2147483648 10000000
 #databaseHost = kolossus-10
 #parasolCommandForAlignment = /hive/users/benedict/parasol -host=swarm-10
+#batchSystemForAlignments = parasol
+#extraJobTreeArgs = --bigBatchSystem singleMachine --bigMemoryThreshold 2147483648 --bigMaxMemory 536870912000 --bigCpuThreshold 4 --bigMaxCpus 25
 
 #########
 #Build basic cactus alignment
@@ -19,7 +22,7 @@ parasolCommandForAlignment = parasol
 pipeline :
 	rm -rf ./jobTree
 	#Running pipeline to build comparisons
-	python ${binPath}/pipeline.py --defaultMemory 8589934593 --databaseHost='${databaseHost}' --batchSystemForAlignments='${batchSystemForAlignments}' --parasolCommandForAlignment='${parasolCommandForAlignment}' --singleCopyIngroup '${singleCopyIngroup}' --gapGamma '${gapGamma}' --constraints ${constraints} --outgroupEvent ${outgroupEvent} --heldOutSequences '${heldOutSequences}' --sampleNumber ${sampleNumber} --permutations '${permutations}' --useSimulatedAnnealing '${useSimulatedAnnealing}' --theta '${theta}' --blastAlignmentStrings '${blastAlignmentStrings}' --baseLevel '${baseLevel}' --maxNumberOfChains '${maxNumberOfChains}' --referenceSpecies '${referenceSpecies}' --haplotypeSequences '${sequences}' --newickTree '${newickTree}' --outputDir ${outputDir} --configFile ${configFile} --referenceAlgorithms '${referenceAlgorithms}' --minimumNsForScaffoldGap ${minimumNsForScaffoldGap} --rangeOfMinimumBlockDegrees '${minimumBlockDegreeRange}' --jobTree ./jobTree ${jobTreeFlags}
+	python ${binPath}/pipeline.py --defaultMemory 8589934593 --databaseHost='${databaseHost}' ${extraJobTreeArgs} --batchSystemForAlignments='${batchSystemForAlignments}' --parasolCommandForAlignment='${parasolCommandForAlignment}' --singleCopyIngroup '${singleCopyIngroup}' --gapGamma '${gapGamma}' --constraints ${constraints} --outgroupEvent ${outgroupEvent} --heldOutSequences '${heldOutSequences}' --sampleNumber ${sampleNumber} --permutations '${permutations}' --useSimulatedAnnealing '${useSimulatedAnnealing}' --theta '${theta}' --blastAlignmentStrings '${blastAlignmentStrings}' --baseLevel '${baseLevel}' --maxNumberOfChains '${maxNumberOfChains}' --referenceSpecies '${referenceSpecies}' --haplotypeSequences '${sequences}' --newickTree '${newickTree}' --outputDir ${outputDir} --configFile ${configFile} --referenceAlgorithms '${referenceAlgorithms}' --minimumNsForScaffoldGap ${minimumNsForScaffoldGap} --rangeOfMinimumBlockDegrees '${minimumBlockDegreeRange}' --jobTree ./jobTree ${jobTreeFlags}
 	jobTreeStatus --jobTree ./jobTree --failIfNotComplete
 	rm -rf ./jobTree
 	
